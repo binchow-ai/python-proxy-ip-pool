b220724382549444841897398baa6668ba03262f
zlikun <zlikun-dev@hotmail.com>
2018-07-20 16:11:46 +0800

    实现查询API

---
+++ api.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import redis
+from flask import Flask, jsonify
+
+import config
+from process import DataProcessor
+
+app = Flask(__name__)
+dp = DataProcessor()
+
+client = redis.StrictRedis(host=config.redis_host,
+                           port=config.redis_port,
+                           decode_responses=True,
+                           charset='utf-8')
+
+
+@app.route('/')
+def index():
+    return 'Nothing !'
+
+
+@app.route('/<protocol>/random')
+def random(protocol):
+    """
+    随机返回一个优质代理IP
+    :param protocol:
+    :return:
+    """
+    return client.srandmember('{}:proxies:{}'.format(protocol, 1))
+
+
+@app.route('/proxies')
+def proxies():
+    """
+    返回全部代理IP
+    :return:
+    """
+    return jsonify([proxy for proxy, _ in dp.query()])
+
+
+@app.route('/<protocol>/proxies')
+def proxies_by_protocol(protocol):
+    """
+    选择符合指定协议的全部代理IP
+    :param protocol:
+    :return:
+    """
+    return jsonify([proxy for proxy, _ in dp.query()
+                    if proxy.startswith('{}://'.format(protocol))])
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8888)
